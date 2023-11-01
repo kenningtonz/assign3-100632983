d5383421fbe21d63a6ea76c1b51d718b582be366
Kennedy Mac <kennedy.adams@dcmail.ca>
2023-11-01 11:40:16 -0400

    Disable Theme Editor

--- app/public/wp-config.php
+++ app/public/wp-config.php
@@ -99,5 +99,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 	define( 'ABSPATH', __DIR__ . '/' );
 }
 
+// disable theme editor
+define( 'DISALLOW_FILE_EDIT', true );
+
 /** Sets up WordPress vars and included files. */
 require_once ABSPATH . 'wp-settings.php';
